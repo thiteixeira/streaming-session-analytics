aa16fda2d53c2a42a24993473bc300bd7547b028
Thiago Teixeira <thiteixeira@gmail.com>
2022-04-07 19:47:20 -0400

    add session duration plot

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ $ source myvenv/bin/activate
 $ pip install -r requirements.txt
 $ python main.py
 ```
+
+The code outputs the distribution of session durations for a specific event (event 2 in this case).
+
+![alt text](./assets/Session_Durations.png)
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+sns.set_theme(style="whitegrid")
+
 BASE_URL = 'https://raw.githubusercontent.com/brightcove/streaming-dataset/main/datasets/'
 HEADER_LIST = ['session', 'seq', 'device_type', 'device_os', 'browser', 
               'player', 'player_width', 'player_height', 
@@ -14,10 +16,13 @@ HEADER_LIST = ['session', 'seq', 'device_type', 'device_os', 'browser',
 
 # Number of files to pull for each event. Can reduce if you want to test
 EVENT_1 = 30
-EVENT_2 = 2 #13
+EVENT_2 = 13
 
 def plot_sessions(df: pd.DataFrame):
-    sns.histplot(data=df, y='seq_s', hue='session')
+    ax = sns.scatterplot(x='seq', y='session', data=df, hue='session', legend=None)
+    ax.set_title('Distribution of Session Lengths')
+    ax.set(xlabel='Session Duration (s)', ylabel='Session ID')
+    plt.legend(title='Event 2', loc='upper right')
     plt.show()
 
 def get_session_info(df: pd.DataFrame):
@@ -27,11 +32,17 @@ def get_session_info(df: pd.DataFrame):
     print(f'Max session length: {max_session_length}')
 
     session_df = df[['session', 'seq']]
-    # each seq number corresponds to 10 seconds that is sent from the player
+    # Find the max seq number for each session
+    session_df = session_df.loc[df.groupby('session')['seq'].idxmax()]
+    session_df.reset_index(inplace=True)
+
+    # Drop rows where seq is less than 300 (50 percentile)
+    session_df.drop(session_df[session_df.seq < 300].index, inplace=True)
+
+    # each seq number corresponds to a 10-second beacon sent from the player
     session_df['seq'] = df['seq'].apply(lambda x: x * 10)
-    session_df.rename(columns={"seq": "seq_s"}, inplace=True)
-    
-    # TODO: drop sessions with duration less than 50 seqs
+    print(session_df)
+
     plot_sessions(session_df)
 
 def fetch_data(event_number: str) -> pd.DataFrame:
@@ -45,6 +56,7 @@ def fetch_data(event_number: str) -> pd.DataFrame:
 
 
 if __name__ == "__main__":
+    # TODO: add argparse to allow user to specify event number
     df = fetch_data(2)
     get_session_info(df)
     

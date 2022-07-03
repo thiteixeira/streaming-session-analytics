c59675929eec6e76c8e68f7322963a7de0389bae
Thiago Teixeira <thiteixeira@gmail.com>
2022-07-03 19:51:55 -0400

    add basic code structure

--- README.md
+++ README.md
@@ -1,15 +1,53 @@
 # Streaming Dataset Analysis
+
 Further analysis of the Streaming Dataset paper
 
 ## Running the code
 
+Create a virtual environment:
+
 ```bash
-$ python3 -m venv myvenv
-$ source myvenv/bin/activate
+$ python3 -m venv .venv
+$ source .venv/bin/activate
 $ pip install -r requirements.txt
-$ python main.py
 ```
 
-The code outputs the distribution of session durations for a specific event (event 2 in this case).
+Run with arguments, where the arguments are the following:
 
-![alt text](./assets/Session_Durations.png)
+- Event Number (--evt-num): The event number to analyze [1, 2, 3, 4]. Default: 1
+- Number of files to pull (--num-files): The number of files to load from GitHub. If no number is given, all files will be loaded
+- Type of analysis (--analysis): **Required.** Which analysis to run. Options are:
+  - "all": Run all analyses
+  - "bitrate": Run the bitrate analysis
+  - "fluctuation": Run the fluctuation analysis
+  - "rate_rebuffering": Run the rate rebuffering analysis
+  - "buffering_ratio": Run the buffering ratio analysis
+
+```bash
+# Run the code with default arguments
+$ ./main.py --analysis=all
+
+# Run the code with custom arguments
+$ ./main.py --evt-num=1 --num-files=1 --analysis=bitrate
+
+# Help
+$ ./main.py --help
+```
+
+## Event Stats
+
+### Average Bitrate
+
+Calculated as the average bitrate of the requested renditions over time.
+
+### Rate of Fluctuations
+
+How often (per minute) the rendition changes
+
+### Rate of Buffering
+
+The number of buffering events per minute over the session duration
+
+### Buffering Ratio
+
+The fraction of time spent in buffering events over the duration of the video session
--- main.py
+++ main.py
@@ -1,62 +1,110 @@
+#!.venv/bin/python
+
+import argparse
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 sns.set_theme(style="whitegrid")
 
-BASE_URL = 'https://raw.githubusercontent.com/brightcove/streaming-dataset/main/datasets/'
-HEADER_LIST = ['session', 'seq', 'device_type', 'device_os', 'browser', 
-              'player', 'player_width', 'player_height', 
-              'rendition_indicated_bps', 'rendition_width', 
-              'rendition_height', 'rendition_framerate', 'video_codec', 
-              'video_codec_profile', 'format', 'segment_duration', 
-              'video_seconds_viewed', 'forward_buffer_seconds', 
-              'rebuffering_seconds', 'rebuffering_count', 
-              'media_bytes_transferred', 'measured_bps']
+EVENT_1 = 38
+EVENT_2 = 12
+EVENT_3 = 1
+EVENT_4 = 4
+BASE_URL = (
+    "https://raw.githubusercontent.com/brightcove/streaming-dataset/main/datasets/"
+)
+HEADER_LIST = [
+    "session",
+    "seq",
+    "device_type",
+    "device_os",
+    "browser",
+    "player",
+    "player_width",
+    "player_height",
+    "rendition_indicated_bps",
+    "rendition_width",
+    "rendition_height",
+    "rendition_framerate",
+    "video_codec",
+    "video_codec_profile",
+    "format",
+    "segment_duration",
+    "video_seconds_viewed",
+    "forward_buffer_seconds",
+    "rebuffering_seconds",
+    "rebuffering_count",
+    "media_bytes_transferred",
+    "measured_bps",
+]
 
-# Number of files to pull for each event. Can reduce if you want to test
-EVENT_1 = 30
-EVENT_2 = 13
 
-def plot_sessions(df: pd.DataFrame):
-    ax = sns.scatterplot(x='seq', y='session', data=df, hue='session', legend=None)
-    ax.set_title('Distribution of Session Lengths')
-    ax.set(xlabel='Session Duration (s)', ylabel='Session ID')
-    plt.legend(title='Event 2', loc='upper right')
-    plt.show()
+def load_data(num_of_files: int, event_num: int = 1) -> pd.DataFrame:
+    """Fetch data from GitHub repo and load it to a dataframe."""
 
-def get_session_info(df: pd.DataFrame):
-    unique_sessions = df['session'].nunique()
-    print(f'Number of unique sessions: {unique_sessions}')
-    max_session_length = df['session'].max()
-    print(f'Max session length: {max_session_length}')
+    if num_of_files == None or event_num == 3:
+        if event_num == 1:
+            num_of_files = EVENT_1
+        elif event_num == 2:
+            num_of_files = EVENT_2
+        elif event_num == 3:
+            num_of_files = EVENT_3
+        elif event_num == 4:
+            num_of_files = EVENT_4
 
-    session_df = df[['session', 'seq']]
-    # Find the max seq number for each session
-    session_df = session_df.loc[df.groupby('session')['seq'].idxmax()]
-    session_df.reset_index(inplace=True)
-
-    # Drop rows where seq is less than 300 (50 percentile)
-    session_df.drop(session_df[session_df.seq < 300].index, inplace=True)
-
-    # each seq number corresponds to a 10-second beacon sent from the player
-    session_df['seq'] = df['seq'].apply(lambda x: x * 10)
-    print(session_df)
-
-    plot_sessions(session_df)
-
-def fetch_data(event_number: str) -> pd.DataFrame:
-    NUM_FILES = EVENT_1 if event_number == 1 else EVENT_2
-    for i in range(NUM_FILES):
-        print(f'Reading csv file {i} ...')
+    for i in range(num_of_files):
+        print(f"Reading csv file {i} ...")
+        url = (
+            f"{BASE_URL}event{event_num}/event{event_num}_playback_statistics_{i}.csv"
+            if event_num != 3
+            else f"{BASE_URL}event{event_num}/event{event_num}_playback_statistics.csv"
+        )
+        df = pd.read_csv(url)
+        if i == 0:
+            df_all = df
+        else:
+            df_all = df_all.append(df)
         skiprows = 1 if i == 0 else 0
-        df = pd.concat([pd.read_csv(f'{BASE_URL}event{event_number}/event{event_number}_playback_statistics_{i}.csv', 
-        names=HEADER_LIST, skiprows=skiprows)], ignore_index=True)
+        df = pd.concat(
+            [
+                pd.read_csv(
+                    url,
+                    names=HEADER_LIST,
+                    skiprows=skiprows,
+                )
+            ],
+            ignore_index=True,
+        )
     return df
 
 
 if __name__ == "__main__":
-    # TODO: add argparse to allow user to specify event number
-    df = fetch_data(2)
-    get_session_info(df)
-    
+    event_parser = argparse.ArgumentParser(
+        description="Define event and number of files"
+    )
+    event_parser.add_argument(
+        "--evt-num",
+        dest="event_number",
+        type=int,
+        choices=[1, 2, 3, 4],
+        help="event number",
+    )
+    event_parser.add_argument(
+        "--num-files",
+        dest="num_files",
+        type=int,
+        help="number of files to read from GitHub",
+    )
+    event_parser.add_argument(
+        "--analysis",
+        dest="analysis",
+        choices=["bitrate", "fluctuation", "rate_rebuffering", "buffering_ratio"],
+        required=True,
+        help="which analysis to run",
+    )
+
+    args = event_parser.parse_args()
+
+    event_df = load_data(num_of_files=args.num_files, event_num=args.event_number)
+    print(event_df.head())
